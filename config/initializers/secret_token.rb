b2538b7a1672b893f6f28ea8cdc77a51e3f96253
Andrea D'Amico <andrea.damico@gmail.com>
2013-12-06 10:16:05 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gardescap::Application.config.secret_key_base = 'c321999b3dfa8e0e0cad77ae9c0c60d50df2709b654fa6c91348a679efec9207c7a9a0809d4bedb448b0bd12eb2974e064bbfa153b287733092733eba5893a78'
