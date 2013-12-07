074761c05568d34d0df3be94a2526b7e497892ba
Andrea D'Amico <andrea.damico@gmail.com>
2013-12-07 15:32:08 +0100

    adding year to gardes months

--- app/controllers/periods_controller.rb
+++ app/controllers/periods_controller.rb
@@ -4,7 +4,7 @@ class PeriodsController < ApplicationController
   def show
     @period = params[:id].present? ? Period.find(params[:id]) : Period.last
     @gardes = @period.gardes
-    @mois_gardes = @gardes.group_by {|garde| I18n.l(garde.date, format: :month_only)}
+    @mois_gardes = @gardes.group_by {|garde| I18n.l(garde.date, format: :month_year)}
     @mois = @mois_gardes.keys
   end
 
