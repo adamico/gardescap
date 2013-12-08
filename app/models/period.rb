d912861645fae33049e986526429feec44127683
Andrea D'Amico <andrea.damico@gmail.com>
2013-12-08 16:32:34 +0100

    utility methods for periods dates

--- app/models/period.rb
+++ app/models/period.rb
@@ -6,4 +6,27 @@ class Period < ActiveRecord::Base
   def gardes
     Garde.where(date: starts_at..ends_at).order(:date)
   end
+
+  def mois
+    months_in_period = ((ends_at - starts_at).to_f / 30).round
+    result = []
+    (0..(months_in_period-1)).each do |i|
+      result << I18n.l(starts_at + i.months, format: :month_year)
+    end
+    result
+  end
+
+  def days(month=nil)
+    if month
+      range.select {|date| date.month == month}
+    else
+      range
+    end
+  end
+
+  private
+
+  def range
+    (starts_at..ends_at).to_a
+  end
 end
--- spec/models/period_spec.rb
+++ spec/models/period_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Period do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#mois" do
+    it "should return an array of 'month-Year' for each month in period" do
+      Period.new(starts_at: "2014-05-01", ends_at: "2014-08-31").mois.should == ["mai-2014", "juin-2014", "juillet-2014", "août-2014"]
+    end
+  end
+
+  describe "#days" do
+    it "should return an array of dates between #starts_at and #ends_at" do
+      start_date = "2014-05-01".to_date
+      end_date = "2014-06-01".to_date
+      Period.new(starts_at: start_date, ends_at: end_date).days.should == (start_date..end_date).to_a
+    end
+    it "should only include dates in month if provided" do
+      start_date = "2014-05-01".to_date
+      end_date = "2014-06-01".to_date
+      Period.new(starts_at: start_date, ends_at: end_date).days(5).should == (start_date..(start_date + 1.month - 1.day)).to_a
+    end
+  end
 end
