a4e5445e48f4375a588155a054bd07d59f41118b
Andrea D'Amico <andrea.damico@gmail.com>
2013-12-09 10:45:02 +0100

    dont use tableaux name in backend

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Gardescap::Application.routes.draw do
 
   get "choix", to: "periods#show"
   get "tableau_actuel", to: "periods#show"
-  resources :periods, as: :tableaux, path: "/tableaux" do
+  resources :periods, path: "/tableaux" do
     get :populate, on: :member
   end
   get "tags", to: "gardes#tags", as: "gardes_tags"
