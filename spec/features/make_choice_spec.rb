452991fd5bb583fbcd162a288a7848acfc5f2990
Andrea D'Amico <andrea.damico@gmail.com>
2014-03-17 17:42:37 +0100

    fix make choice specs

--- spec/features/make_choice_spec.rb
+++ spec/features/make_choice_spec.rb
@@ -1,51 +1,55 @@
 require "spec_helper"
 
-feature "Un médecin du tour de garde souhaitant participer au tour de garde" do
+feature "Un médecin souhaitant participer au tour de garde" do
 
   let(:user)    { create(:user, name: "martin") }
   let(:user2)    { create(:user, name: "dupond") }
   let(:user3)    { create(:user, name: "durand") }
   let(:user4)    { create(:user, name: "dumas") }
-  let!(:period) { create(:period, starts_at: "2014-01-01", ends_at: "2014-01-31") }
-  let!(:garde)  { create(:garde, period: period, date: "2014-01-01", time: "jj")}
-  let!(:garde2)  { create(:garde, period: period, date: "2014-01-15", time: "jj")}
-  let!(:garde3)  { create(:garde, period: period, date: "2014-01-30", time: "jj")}
-  let!(:garde4)  { create(:garde, period: period, date: "2014-01-07", time: "jj")}
 
   background do
     login user
-    create(:assignment, garde: garde2, user: user2)
-    create(:assignment, garde: garde3, user: user)
-    create(:assignment, garde: garde4, user: user2)
-    create(:assignment, garde: garde4, user: user3)
-    create(:assignment, garde: garde4, user: user4)
-    visit choix_path
   end
 
-  scenario "peut se rajouter sur une garde vide", js: true do
-    click_on "Choisir la garde - jj - 01/01/2014"
-    expect(page).to have_text "martin"
+  context "si le tableau est bouclé" do
+    background do
+      period = create(:period, starts_at: "2014-01-01", ends_at: "2014-01-31", state: "closed")
+      garde = create(:garde, period: period, date: "2014-01-01", time: "jj")
+      create(:assignment, user: user, garde: garde)
+      visit choix_path
+    end
+    scenario "ne peut pas choisir des plages", js: true do
+      expect(page).not_to have_link "Choisir la garde - jj - 01/01/2014"
+    end
   end
 
-  scenario "peut s'enlever d'une plage", js: true do
-    click_on "martin"
-    expect(page).not_to have_text "martin"
-  end
+  context "si le tableau est actif" do
+    let(:period) {create(:period, starts_at: "2014-01-01", ends_at: "2014-01-31")}
+    let!(:garde)  {create(:garde, period: period, date: "2014-01-01", time: "jj")}
+    let!(:garde2) {create(:garde, period: period, date: "2014-01-30", time: "jj")}
+    let!(:garde4) {create(:garde, period: period, date: "2014-01-07", time: "jj")}
+    background do
+      create(:assignment, garde: garde2, user: user)
+      [user2, user3, user4].each do |theuser|
+        create(:assignment, garde: garde4, user: theuser)
+      end
+      visit choix_path
+    end
+    scenario "peut se rajouter sur une plage vide", js: true do
+      click_on "Choisir la garde - jj - 01/01/2014"
+      expect(page).to have_link "Enlever martin de la Journée/AM Junior du 01/01/2014"
+    end
 
-  scenario "peut se rajouter sur une plage non pleine", js: true do
-    click_on "Choisir la garde - jj - 15/01/2014"
-    expect(page).to have_text "dupond, martin"
-  end
+    scenario "peut s'enlever d'une plage", js: true do
+      click_on "Enlever martin de la Journée/AM Junior du 30/01/2014"
+      expect(page).not_to have_link "Enlever martin de la Journée/AM Junior du 30/01/2014"
+    end
 
-  scenario "ne peut pas se rajouter sur une plage pleine" do
-    expect(page).not_to have_link "Choisir la garde - jj - 07/01/2014"
-  end
-
-  scenario "peut clicker sur son nom" do
-    expect(page).to have_link "martin"
-  end
-
-  scenario "ne doit pas pouvoir clicker sur le nom d'un autre candidat" do
-    expect(page).not_to have_link "dupond"
+    scenario "ne peut pas se rajouter sur une plage pleine" do
+      [user2, user3, user4].map(&:name).each do |theusername|
+        expect(page).to have_content(theusername)
+      end
+      expect(page).not_to have_link "Choisir la garde - jj - 07/01/2014"
+    end
   end
 end
