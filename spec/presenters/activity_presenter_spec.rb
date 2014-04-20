398cf4e73285accbe3c915a7adc6023c5ee4d49a
Andrea D'Amico <andrea.damico@gmail.com>
2014-04-20 20:49:52 +0200

    really prepare activity presenter spec

--- spec/presenters/activity_presenter_spec.rb
+++ spec/presenters/activity_presenter_spec.rb
@@ -4,11 +4,13 @@ require 'activity_presenter'
 describe ActivityPresenter do
   let(:user)       { create(:user) }
   let(:garde)      { create(:garde, date: "2014-05-01", time: "nl") }
-
-  subject { ActivityPresenter.new(activity, view) }
+  let(:assignment) { create(:assignment, garde: garde, user: user) }
 
   describe "#render_activity_update" do
     context "for assignment creation" do
+      let(:activity)   { assignment.create_activity(:create, owner: user) }
+      subject { ActivityPresenter.new(activity, view) }
+
       it "exists" do
         expect(subject).to respond_to(:render_activity_update)
       end
