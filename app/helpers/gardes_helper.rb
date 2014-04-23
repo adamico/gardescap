590c4bb6f7d1a9596c3bbb6838b12c50a181201f
Andrea D'Amico <andrea.damico@gmail.com>
2014-04-23 16:39:16 +0200

    can toggle holiday with a button

--- app/helpers/gardes_helper.rb
+++ app/helpers/gardes_helper.rb
@@ -2,6 +2,19 @@ module GardesHelper
   def link_to_create_or_destroy_garde(garde, date, time, period)
     ActionLink.new(garde, date, time, period, self).to_html
   end
+
+  def link_to_toggle_holiday_garde(garde)
+    holiday = garde.holiday? ? false : true
+    icon_class = garde.holiday? ? "heart-empty" : "heart"
+    titre = garde.holiday? ? "normale" : "fériée"
+    form_for garde, role: "form", remote: true do |f|
+      f.hidden_field(:holiday, value: holiday) +
+      button_tag(type: :submit, class: "btn btn-xs btn-default", id: "toggle-holiday-#{garde.time}-#{garde.date}", titre: "Rendre #{titre} garde - #{garde.time} - #{l(garde.date)}") do
+        content_tag(:span, nil, class: "glyphicon glyphicon-#{icon_class}")
+      end
+    end
+  end
+
 end
 
 class ActionLink
@@ -19,7 +32,7 @@ class ActionLink
   end
 
   def to_html
-    link_to icon, path, method: link_method, remote: true, class: "btn btn-xs btn-#{link_class}",
+    link_to icon, path, method: link_method, remote: true, class: "pull-left btn btn-xs btn-#{link_class}",
       data: dom_data, title: "#{title_prefix} garde - #{time_date}" if context.can?(action, Garde)
   end
 
